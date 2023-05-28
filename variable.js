fd4891b0cab42b941faf672eb7573d27f246cf72
CodeByTanvir01~ <tanvirh484@icloud.com>
2023-05-28 23:10:19 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,7 @@
+var momGivenMoney = 1000;
+var orangePrice = 350;
+var applePrice = 350;
+var totalPrice = orangePrice + applePrice;
+var getBack = momGivenMoney - totalPrice;
+
+console.log(getBack)
